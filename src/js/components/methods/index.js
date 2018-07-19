d668b1876ce21998eb17d09f00c46ae47022e05d
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-19 20:40:13 +0200

    removed cached

--- src/js/components/methods/index.js
+++
@@ -1,5 +0,0 @@
-/* @flow */
-'use strict';
-
-export { default as GetPublicKey } from './GetPublicKey';
-export { default as ComposeTransaction } from './ComposeTransaction';
--- src/js/store/index.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-'use strict';
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports = require('./store.dev'); // eslint-disable-line global-require
-} else {
-    module.exports = require('./store.dev'); // eslint-disable-line global-require
-}
