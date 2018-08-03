87de19f4a307907002cd3d3d019fc705478e3178
Szymon Lesisz <sz.lesisz@gmail.com>
2018-08-03 16:06:42 +0200

    webextension files

---
+++ background/index.js
@@ -0,0 +1,23 @@
+/**
+When the button's clicked:
+- call for TrezorConnect action
+- show a notification with response (if succeed)
+*/
+
+function onClick() {
+    // window.__TREZOR_CONNECT_SRC = 'http://localhost:8082/';
+    window.TrezorConnect.getAddress({ 
+        path: "m/49'/0'/0'/0/0"
+    }).then(response => {
+        const message = response.success ? `BTC Address: ${ response.payload.address }` : `Error: ${ response.payload.error }`;
+        chrome.notifications.create(new Date().getTime().toString(), {
+            type: 'basic',
+            iconUrl: 'icons/48.png',
+            title: 'TrezorConnect',
+            message
+        });
+    }).catch(error => {
+        console.error("TrezorConnectError", error)
+    });
+}
+chrome.browserAction.onClicked.addListener(onClick);
